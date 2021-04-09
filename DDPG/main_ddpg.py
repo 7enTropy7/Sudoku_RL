4df53b9ae6a1df1d6a2366171b6bba709777b10f
Anirudh Rajiv Menon <36445587+axe76@users.noreply.github.com>
2021-04-09 23:41:00 +0530

    Add files via upload

--- DDPG/main_ddpg.py
+++ DDPG/main_ddpg.py
@@ -17,16 +17,27 @@ if __name__ == '__main__':
     for i in range(n_games):
         observation = np.array(env.reset())
         done = False
+        c = 1
 
         while not done:
             env.render()
             action = agent.choose_action(observation, evaluate)
             observation_, reward, done = env.step(action)
+            
+            c += 1
+            if (c%2000 == 0):
+                observation = np.array(env.reset())
+                env.reward = -100
+                reward = env.reward
+                
             print('Action : ',action)
             print('Reward ', reward)
+            
             agent.remember(observation, action, reward, observation_, done)
             agent.learn()
             observation = observation_
+            
+            print('Timestep : ',c)
             
 
         
